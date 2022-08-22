a0bee58ed8f2259be9d2425eb96927536168d848
dantremb <dantremb@student.42quebec.com>
2022-08-22 08:17:33 -0400

    22 August 08:17:33

---
+++ cpp01/ex00/srcs/ Zombie.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*    Zombie.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dantremb <dantremb@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/19 21:58:26 by dantremb          #+#    #+#             */
+/*   Updated: 2022/08/22 08:15:15 by dantremb         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+class Zombie
+{
+	
+public:
+
+	// Constructors and destructors
+
+
+private:
+
+	
+};
+
+#endif
